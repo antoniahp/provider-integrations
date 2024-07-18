ddd7c89ca53ba6c4cc2f7870a2442331edfa3bec
antoniahp <a.herrerapuertas@gmail.com>
2024-07-18 13:08:10 +0200

    query structure and implementation

---
+++ app/hotels/infraestructure/views/get_hotels/get_hotel_serializer.py
@@ -0,0 +1,16 @@
+from hotels.domain.hotel import Hotel
+
+
+class GetHotelSerializer:
+    def serialize(self, hotel:Hotel) -> dict:
+        return {
+            "id": hotel.id,
+            "hotel_name": hotel.hotel_name,
+            "city": hotel.city,
+            "address": hotel.address,
+            "rating": hotel.rating,
+            "availability":hotel.availability,
+            "price_per_night": hotel.price_per_night,
+            "amenity": hotel.amenity
+
+        }
---
+++ app/hotels/infraestructure/views/get_hotels/get_hotel_view.py
@@ -0,0 +1,8 @@
+from django.views import View
+
+class GetHotelView(View):
+    def __init__(self):
+        pass
+
+    def get_hotel(self, request):
+        pass
