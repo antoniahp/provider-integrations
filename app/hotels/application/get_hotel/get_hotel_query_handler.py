82ef3b9bc7fcfc4b03f90074c223a6948712437b
antoniahp <a.herrerapuertas@gmail.com>
2024-07-18 19:29:43 +0200

    django view

---
+++ app/hotels/application/get_hotel/get_hotel_query_handler.py
@@ -0,0 +1,13 @@
+from cqrs.queries.query_handler import QueryHandler
+from cqrs.queries.query_response import QueryResponse
+from hotels.application.get_hotel.get_hotel_query import GetHotelsQuery
+from hotels.domain.hotel_repository import HotelRepository
+
+
+class GetHotelsQueryHandler(QueryHandler):
+    def __init__(self, hotel_repository: HotelRepository):
+        self.hotel_repository = hotel_repository
+
+    def handle(self, query: GetHotelsQuery) -> QueryResponse:
+        hotels = self.hotel_repository.get_all_hotels()
+        return QueryResponse(content=hotels)
---
+++ app/hotels/infraestructure/views/get_hotels/get_hotel_view.py
@@ -0,0 +1,24 @@
+from django.http import JsonResponse
+from django.views import View
+
+from hotels.application.get_hotel.get_hotel_query import GetHotelsQuery
+from hotels.application.get_hotel.get_hotel_query_handler import GetHotelsQueryHandler
+from hotels.infraestructure.db_hotel_repository import DbHotelRepository
+
+from hotels.infraestructure.views.get_hotels.get_hotel_serializer import GetHotelSerializer
+
+
+class GetHotelView(View):
+    def __init__(self):
+        self.__hotel_repository = DbHotelRepository()
+        self.__get_hotels_query_handler = GetHotelsQueryHandler(hotel_repository = self.__hotel_repository)
+        self.__get_hotel_serializer = GetHotelSerializer()
+
+    def get(self,request):
+
+        query = GetHotelsQuery()
+        query_response = self.__get_hotels_query_handler.handle(query)
+        hotels = query_response.content
+        return JsonResponse({
+            "hotels": self.__get_hotel_serializer.serialize(hotels)
+        }, status=200)
