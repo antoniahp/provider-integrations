4e297779c2ced6c3787aecdbe5d8dfc59111e824
antoniahp <a.herrerapuertas@gmail.com>
2024-07-31 13:16:47 +0200

    implemented deb_repository and view

---
+++ app/hotels/infraestructure/views/webhook_reviews/webhook_reviews_view.py
@@ -0,0 +1,38 @@
+import json
+from datetime import date, datetime
+from decimal import Decimal
+
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+from hotels.application.add_reviews.add_review_command import AddReviewCommand
+from hotels.application.add_reviews.add_review_command_handler import AddReviewCommandHandler
+from hotels.domain.review_creator import ReviewCreator
+from hotels.infraestructure.db_hotel_repository import DbHotelRepository
+from hotels.infraestructure.db_review_repository import DbReviewRepository
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class WebhookReviewsView(View):
+    def __init__(self):
+        super().__init__()
+        self.__db_review_repository = DbReviewRepository()
+        self.__review_creator = ReviewCreator()
+        self.__db_hotel_repository = DbHotelRepository()
+        self.__add_reviews_command_handler = AddReviewCommandHandler(review_repository=self.__db_review_repository, review_creator=self.__review_creator, hotel_repository=self.__db_hotel_repository)
+
+
+    def post(self, request):
+        data = json.loads(request.body)
+        command = AddReviewCommand(
+            hotel_name=data.get("hotel_name"),
+            user_name=data.get("user_name"),
+            review=Decimal(data.get("review")),
+            title=data.get("title"),
+            text=data.get("text"),
+            published_at=datetime.strptime(data.get("published_at"), "%Y-%m-%d").date()
+        )
+        self.__add_reviews_command_handler.handle(command)
+        return HttpResponse(status=201)
--- app/hotels/urls.py
+++ app/hotels/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from hotels.infraestructure.views.get_hotels.get_hotel_view import GetHotelView
+from hotels.infraestructure.views.webhook_reviews.webhook_reviews_view import WebhookReviewsView
 
 urlpatterns = [
-    path("",GetHotelView.as_view(), name="hotel")
+    path("reviews/", WebhookReviewsView.as_view(), name="reviews"),
+    path("",GetHotelView.as_view(), name="hotel"),
 
 ]
