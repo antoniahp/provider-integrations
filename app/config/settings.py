6b308906701ec6799b80414cb3d5806b7554efc4
antoniahp <a.herrerapuertas@gmail.com>
2024-08-01 15:26:15 +0200

    implement mailing

--- app/config/settings.py
+++ app/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +135,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#MAILING
+
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.devmail.email'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
--- app/hotels/application/export_top_hotels/export_top_hotels_command_handler.py
+++ app/hotels/application/export_top_hotels/export_top_hotels_command_handler.py
@@ -1,18 +1,28 @@
+
+
+from config import settings
 from cqrs.commands.command_handler import CommandHandler
 from hotels.application.export_top_hotels.export_top_hotels_command import ExportTopHotelsCommand
 from hotels.domain.hotel_repository import HotelRepository
 import csv
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+
+
+
 
 class ExportTopHotelsCommandHandler(CommandHandler):
     def __init__(self, hotel_repository: HotelRepository):
         self.hotel_repository = hotel_repository
 
+
     def handle(self, command: ExportTopHotelsCommand):
-        # obtener los hoteles
+        # Obtener los hoteles
         hotels = self.hotel_repository.get_all_hotels()
 
-        # parsear hoteles
-        with open('top_hotels.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        # Parsear hoteles
+        csv_file_path = 'top_hotels.csv'
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
             csv_writer = csv.writer(csvfile, delimiter=',')
             csv_writer.writerow(['ID', 'Name', 'Address', 'Rating', 'City', 'Amenities'])
             for hotel in hotels:
@@ -26,7 +36,38 @@ class ExportTopHotelsCommandHandler(CommandHandler):
                         hotel.amenity,
                     ])
 
-        # enviar mail
+        # Enviar el email
+        email = EmailMessage(
+            "Informe semanal",
+            "Adjunto tiene un informe donde observará los mejores hoteles del sistema",
+            settings.DEFAULT_FROM_EMAIL,
+            ["antoniaherrera620@gmail.com"],
+        )
+        email.attach_file(csv_file_path)
+        email.send()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Enviar correo
+#             email = EmailMessage(
+#                 subject='Hoteles Recomendados',
+#                 body='Adjunto encontrarás el archivo con la lista de hoteles recomendados.',
+#                 from_email=settings.DEFAULT_FROM_EMAIL,
+#                 to=['antoniaherrera620@gmail.com'],
+#             )
+#             email.attach_file(hotels_csv)
+#             email.send(fail_silently=False)
+
 
 
 
