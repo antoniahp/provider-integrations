b5588db4e8cd846524626571bf7a24af077044e7
antoniahp <a.herrerapuertas@gmail.com>
2024-08-01 16:57:04 +0200

    refactor

--- app/hotels/domain/hotel_repository.py
+++ app/hotels/domain/hotel_repository.py
@@ -8,16 +8,13 @@ class HotelRepository(ABC):
     def get_all_hotels(self) -> Sequence[Hotel]:
         pass
 
-
     @abstractmethod
     def filter_hotels_by_name(self, hotel_name:str) -> Sequence[Hotel]:
         pass
 
-
     @abstractmethod
     def filter_hotels_by_rating_gte(self, rating_gte:float) -> Sequence[Hotel]:
         pass
-
 
     @abstractmethod
     def save_hotel(self , hotel: Hotel) -> None:
